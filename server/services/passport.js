fa3e68c40e5da9ccab549377a18de9cc407040ae
ezheng76 <ez328167@gmail.com>
2023-11-05 22:30:46 -0500

    tweaked the passport file to resolve 401 client not auth error

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const mongoose = require('mongoose')
-const keys = require('../config/keys')
+const keys = require('../config/prod')
 const googleStrategy = require('passport-google-oauth20').Strategy
 
 const User = mongoose.model('users')
